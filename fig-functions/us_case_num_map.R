22e82ed3209103effd4bc223e49e694bb3d29eea
rbolt13 <randibolt93@gmail.com>
2023-05-26 09:40:43 -0700

    update us_case_num_map.R style

--- fig-functions/us_case_num_map.R
+++ fig-functions/us_case_num_map.R
@@ -24,21 +24,53 @@ us_case_num_map <- function(){
       lat, 
       group = group,
       # fill = total cases
-      fill = total_cases)) + 
+      fill = total_cases,
+      text = paste("State:", state, 
+                   "\nTotal Cases:",
+                   base::formatC(
+                     total_cases,
+                     format = "d",
+                     big.mark = ","),
+                   "\nPercent of Population: ",
+                   base::round(
+                     total_cases_perc*100,
+                     digits=2), "%"
+                   )
+      )) + 
     ggplot2::geom_polygon(color = "grey") +
     ggplot2::coord_quickmap() + 
     # plot theme
     ggplot2::theme_minimal() +
     # labs
     ggplot2::labs(
-      title = "Total Number of Cases by State",
-      subtitle = "This map shows the total number of covid cases by state.",
-      caption = "Graph by Randi Bolt | Data from NYT and Tidycensus | Period 2020 - 2023.",
+      title = "Total Covid Cases By State",
       x = "",
       y = ""
     ) +
-    # theme
+    # Subtitle and Footnote
+    annotate(
+      "text", x = -100, y = 60, 
+      label = "Discover the distribution of Covid-19 cases across different states with this interactive map.\n Simply hover over the map and utilize the crosshairs to draw a box around an area of interest to zoom in\n and examine the data at a closer level. To zoom out, double click on the map.") +
+    annotate(
+      "text", x = -100, y = 15, 
+      label = "Graph by Randi Bolt\n Data from the NYT and Tidycensus Package\n Data Period: Jan 21, 2020 - March 23, 2023.") +
+    # Legend
+    ggplot2::scale_fill_gradientn(
+      colours = hcl.colors(5),
+      name = "Total Number of Cases",
+      breaks = c(3000000, 6000000, 9000000, 12000000),
+      labels = c("3,000,000", "6,000,000", "9,000,000", "12,000,000")) +
+  # theme
     theme(
-      text = element_text(size = 16, family = "Times")) 
+      text = element_text(size = 16, family = "Times"),
+      plot.title = element_text(
+        hjust = .45,
+        face = "bold"),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      panel.background = element_rect(fill = "white"),
+      panel.grid.major = element_line(color = "white")
+      )
     return(vis)
 }
