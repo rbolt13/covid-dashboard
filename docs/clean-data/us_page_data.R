674299e54322ce93218ff7874dbf2557112529de
rbolt13 <randibolt93@gmail.com>
2023-05-25 17:26:33 -0700

    update

--- docs/clean-data/us_page_data.R
+++ docs/clean-data/us_page_data.R
@@ -16,6 +16,16 @@ base::library(tidyverse)
 # read_csv(): Reads csv file. 
 us_totals <- readr::read_csv("clean-data/us_totals.csv")
 
+#### Row Total Values ####
+us_total_cases <- sum(us_totals$total_cases)
+us_cases_perc <- base::round(
+  (us_total_cases / sum(us_totals$population))*100,
+  digits = 2)
+us_total_deaths <- sum(us_totals$total_deaths)
+us_deaths_perc <- base::round(
+  (us_total_deaths / sum(us_totals$population))*100,
+  digits = 2)
+
 #### Arrange Data####
 # arrange by case
 arr_totals_c <- us_totals %>%
@@ -41,21 +51,9 @@ most_c_val <- base::formatC(
   format = "d",
   big.mark = ",")
 
-sec_most_c <- arr_totals_c[[2,2]]
-sec_most_c_val <- base::formatC(
-  arr_totals_c[[2,3]],
-  format = "d",
-  big.mark = ",")
-
 least_c <- arr_totals_c[[52,2]]
 least_c_val <- base::formatC(
   arr_totals_c[[52,3]],
-  format = "d",
-  big.mark = ",")
-
-sec_least_c <- arr_totals_c[[51,2]]
-sec_least_c_val <- base::formatC(
-  arr_totals_c[[51,3]],
   format = "d",
   big.mark = ",")
 
@@ -64,19 +62,9 @@ most_p_val <- base::round(
   arr_totals_p[[1,4]]*100,
   digits = 2)
 
-sec_most_p <- arr_totals_p[[2,2]]
-sec_most_p_val <- base::round(
-  arr_totals_p[[2,4]]*100,
-  digits = 2)
-
 least_p <- arr_totals_p[[52,2]]
 least_p_val <- base::round(
   arr_totals_p[[52,4]]*100,
-  digits = 2)
-
-sec_least_p <- arr_totals_p[[51,2]]
-sec_least_p_val <- base::round(
-  arr_totals_p[[51,4]]*100,
   digits = 2)
 
 #### Page 2: Deaths ####
@@ -86,21 +74,9 @@ most_d_val <- base::formatC(
   format = "d",
   big.mark = ",")
 
-sec_most_d <- arr_totals_d[[2,2]]
-sec_most_d_val <- base::formatC(
-  arr_totals_d[[2,3]],
-  format = "d",
-  big.mark = ",")
-
 least_d <- arr_totals_d[[52,2]]
 least_d_val <- base::formatC(
   arr_totals_d[[52,3]],
-  format = "d",
-  big.mark = ",")
-
-sec_least_d <- arr_totals_d[[51,2]]
-sec_least_d_val <- base::formatC(
-  arr_totals_d[[51,3]],
   format = "d",
   big.mark = ",")
 
@@ -109,32 +85,23 @@ most_dp_val <- base::round(
   arr_totals_dp[[1,4]]*100,
   digits = 2)
 
-sec_most_dp <- arr_totals_dp[[2,2]]
-sec_most_dp_val <- base::round(
-  arr_totals_dp[[2,4]]*100,
-  digits = 2)
-
 least_dp <- arr_totals_dp[[52,2]]
 least_dp_val <- base::round(
   arr_totals_dp[[52,4]]*100,
   digits = 2)
 
-sec_least_dp <- arr_totals_dp[[51,2]]
-sec_least_dp_val <- base::round(
-  arr_totals_dp[[51,4]]*100,
-  digits = 2)
-
 #### US Page Data ####
 us_page_data <- base::data.frame(
-  state = c(most_c, sec_most_c, least_c, sec_least_c,
-            most_p, sec_most_p, least_p, sec_least_p,
-            most_d, sec_most_d, least_d, sec_least_d,
-            most_dp, sec_most_dp, least_dp, sec_least_dp),
-  val = c(most_c_val, sec_most_c_val, least_c_val, sec_least_c_val,
-          most_p_val, sec_most_p_val, least_p_val, sec_least_p_val,
-          most_d_val, sec_most_d_val, least_d_val, sec_least_d_val,
-          most_dp_val, sec_most_dp_val, least_dp_val, sec_least_dp_val)
-)
+  state = c(most_c, least_c, most_p, least_p, 
+            most_d, least_d, most_dp, least_dp),
+  val = c(most_c_val, least_c_val, most_p_val, least_p_val, 
+          most_d_val, least_d_val, most_dp_val, least_dp_val))
+
+#### US Page Totals Data ####
+us_page_totals <- base::data.frame(
+  cases = c(us_total_cases, us_cases_perc),
+  deaths = c(us_total_deaths, us_deaths_perc))
 
 #### Save Data ####
 utils::write.csv(us_page_data, "clean-data/us_page_data.csv")
+utils::write.csv(us_page_totals, "clean-data/us_page_totals.csv")
